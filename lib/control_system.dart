b397e0d0f871e78adb75822328b745a14f71f136
Amirreza Madani <madani_amirreza@yahoo.com>
2021-12-14 14:13:08 +0330

    feat: add basic project structure

---
+++ lib/control_system.dart
@@ -0,0 +1,6 @@
+// Copyright 2021 - 2021, Amirreza Madani. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Flutter control system widgets, like on-off controller.
+library control_system;
