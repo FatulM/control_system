6960769a65e669ea45cb1e934a74c83f25805fe5
Amirreza Madani <madani_amirreza@yahoo.com>
2021-12-22 14:07:15 +0330

    test: refactor tests

--- test/control_system_test.dart
+++ test/control_system_test.dart
@@ -3,72 +3,74 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('OnOffControllerBuilder', (tester) async {
-    double _value = 10.0;
-    late StateSetter _updater;
+  group('OnOffControllerBuilder', () {
+    testWidgets('basic interaction', (tester) async {
+      double _value = 10.0;
+      late StateSetter _updater;
 
-    Future<void> check(bool state) async {
-      await tester.pumpAndSettle();
-      expect(find.text('state: ${state ? 'on' : 'off'}'), findsOneWidget);
-    }
+      Future<void> check(bool state) async {
+        await tester.pumpAndSettle();
+        expect(find.text('state: ${state ? 'on' : 'off'}'), findsOneWidget);
+      }
 
-    void update(double newValue) {
-      _updater(() => _value = newValue);
-    }
+      void update(double newValue) {
+        _updater(() => _value = newValue);
+      }
 
-    await tester.pumpWidget(
-      MaterialApp(
-        title: 'Testing',
-        theme: ThemeData.light(),
-        home: Scaffold(
-          body: StatefulBuilder(
-            builder: (context, setState) {
-              _updater = setState;
+      await tester.pumpWidget(
+        MaterialApp(
+          title: 'Testing',
+          theme: ThemeData.light(),
+          home: Scaffold(
+            body: StatefulBuilder(
+              builder: (context, setState) {
+                _updater = setState;
 
-              return OnOffControllerBuilder(
-                initialState: false,
-                value: _value,
-                lowerLimit: 40.0,
-                upperLimit: 60.0,
-                builder: (context, state) {
-                  return Center(
-                    child: Text(
-                      'state: ${state ? 'on' : 'off'}',
-                    ),
-                  );
-                },
-              );
-            },
+                return OnOffControllerBuilder(
+                  initialState: false,
+                  value: _value,
+                  lowerLimit: 40.0,
+                  upperLimit: 60.0,
+                  builder: (context, state) {
+                    return Center(
+                      child: Text(
+                        'state: ${state ? 'on' : 'off'}',
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ),
-      ),
-    );
+      );
 
-    // value = 10.0
-    await check(false);
+      // value = 10.0
+      await check(false);
 
-    update(30.0);
-    await check(false);
+      update(30.0);
+      await check(false);
 
-    update(50.0);
-    await check(false);
+      update(50.0);
+      await check(false);
 
-    update(70.0);
-    await check(true);
+      update(70.0);
+      await check(true);
 
-    update(90.0);
-    await check(true);
+      update(90.0);
+      await check(true);
 
-    update(70.0);
-    await check(true);
+      update(70.0);
+      await check(true);
 
-    update(50.0);
-    await check(true);
+      update(50.0);
+      await check(true);
 
-    update(30.0);
-    await check(false);
+      update(30.0);
+      await check(false);
 
-    update(10.0);
-    await check(false);
+      update(10.0);
+      await check(false);
+    });
   });
 }
