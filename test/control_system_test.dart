c9f27898e5ced4845e20b62aad475d0b6dadeace
Amirreza Madani <madani_amirreza@yahoo.com>
2021-12-26 14:04:29 +0330

    test: refactor tests

--- test/control_system_test.dart
+++ test/control_system_test.dart
@@ -4,165 +4,170 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('OnOffControllerBuilder', () {
-    test('diagnosis should provide correct information', () {
-      final widget = OnOffControllerBuilder(
-        upperLimit: 60.0,
-        lowerLimit: 40.0,
-        value: 10.0,
-        builder: (context, state) => Container(),
-      );
-      final element = widget.createElement();
-      final state = element.state;
-      final diagnosticsNode = state.toDiagnosticsNode();
-      final properties = diagnosticsNode.getProperties();
+    group('dart-tests', () {
+      test('diagnosis should provide correct information', () {
+        final widget = OnOffControllerBuilder(
+          upperLimit: 60.0,
+          lowerLimit: 40.0,
+          value: 10.0,
+          builder: (context, state) => Container(),
+        );
+        final element = widget.createElement();
+        final state = element.state;
+        final diagnosticsNode = state.toDiagnosticsNode();
+        final properties = diagnosticsNode.getProperties();
 
-      expect(
-        properties.map((e) => e.toStringDeep()).toList(),
-        containsAll([
-          'listener: null - has NOT change listener',
-          'isResetStateOnBoundaryUpdate: true - resets controller state on widget boundary properties updates',
-          'isResetStateOnInitialStateUpdate: true - resets controller state on widget initial state property updates',
-          'isCallChangeListenerForInitialState: true - calls change listener for initial state',
-          'initialState: false',
-          'upperLimit: 60.0',
-          'lowerLimit: 40.0',
-          'value: 10.0',
-        ]),
-      );
+        expect(
+          properties.map((e) => e.toStringDeep()).toList(),
+          containsAll([
+            'listener: null - has NOT change listener',
+            'isResetStateOnBoundaryUpdate: true - resets controller state on widget boundary properties updates',
+            'isResetStateOnInitialStateUpdate: true - resets controller state on widget initial state property updates',
+            'isCallChangeListenerForInitialState: true - calls change listener for initial state',
+            'initialState: false',
+            'upperLimit: 60.0',
+            'lowerLimit: 40.0',
+            'value: 10.0',
+          ]),
+        );
+      });
     });
 
-    testWidgets('basic interaction should work correctly', (tester) async {
-      double _value = 10.0;
-      late StateSetter _updater;
+    group('widget-tests', () {
+      late bool isResetStateOnBoundaryUpdate;
+      late bool isResetStateOnInitialStateUpdate;
+      late bool isCallChangeListenerForInitialState;
+      late bool initialState;
+      late double value;
+      late double lowerLimit;
+      late double upperLimit;
+
+      WidgetTester? widgetTester;
+      StateSetter? stateSetter;
+      OnOffControllerChangeListener? listener;
+
+      setUp(() {
+        isResetStateOnBoundaryUpdate = true;
+        isResetStateOnInitialStateUpdate = true;
+        isCallChangeListenerForInitialState = true;
+        initialState = false;
+        value = 10.0;
+        lowerLimit = 40.0;
+        upperLimit = 60.0;
+
+        widgetTester = null;
+        stateSetter = null;
+        listener = null;
+      });
+
+      tearDown(() {
+        widgetTester = null;
+        stateSetter = null;
+        listener = null;
+      });
+
+      Future<void> initialize(WidgetTester tester) async {
+        widgetTester = tester;
+
+        await widgetTester!.pumpWidget(
+          MaterialApp(
+            title: 'Testing',
+            theme: ThemeData.light(),
+            home: Scaffold(
+              body: StatefulBuilder(
+                builder: (context, setState) {
+                  stateSetter = setState;
+
+                  return OnOffControllerBuilder(
+                    isCallChangeListenerForInitialState: isCallChangeListenerForInitialState,
+                    isResetStateOnBoundaryUpdate: isResetStateOnBoundaryUpdate,
+                    isResetStateOnInitialStateUpdate: isResetStateOnInitialStateUpdate,
+                    initialState: initialState,
+                    value: value,
+                    lowerLimit: lowerLimit,
+                    upperLimit: upperLimit,
+                    listener: listener,
+                    builder: (context, state) {
+                      return Center(
+                        child: Text(
+                          'state: ${state ? 'on' : 'off'}',
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+        );
+      }
 
       Future<void> check(bool state) async {
-        await tester.pumpAndSettle();
+        await widgetTester!.pumpAndSettle();
         expect(find.text('state: ${state ? 'on' : 'off'}'), findsOneWidget);
       }
 
       void update(double newValue) {
-        _updater(() => _value = newValue);
+        stateSetter!(() => value = newValue);
       }
 
-      await tester.pumpWidget(
-        MaterialApp(
-          title: 'Testing',
-          theme: ThemeData.light(),
-          home: Scaffold(
-            body: StatefulBuilder(
-              builder: (context, setState) {
-                _updater = setState;
+      testWidgets('basic interaction should work correctly', (tester) async {
+        await initialize(tester);
 
-                return OnOffControllerBuilder(
-                  initialState: false,
-                  value: _value,
-                  lowerLimit: 40.0,
-                  upperLimit: 60.0,
-                  builder: (context, state) {
-                    return Center(
-                      child: Text(
-                        'state: ${state ? 'on' : 'off'}',
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ),
-      );
+        // value = 10.0
+        await check(false);
 
-      // value = 10.0
-      await check(false);
+        update(30.0);
+        await check(false);
 
-      update(30.0);
-      await check(false);
+        update(50.0);
+        await check(false);
 
-      update(50.0);
-      await check(false);
+        update(70.0);
+        await check(true);
 
-      update(70.0);
-      await check(true);
+        update(90.0);
+        await check(true);
 
-      update(90.0);
-      await check(true);
+        update(70.0);
+        await check(true);
 
-      update(70.0);
-      await check(true);
+        update(50.0);
+        await check(true);
 
-      update(50.0);
-      await check(true);
+        update(30.0);
+        await check(false);
 
-      update(30.0);
-      await check(false);
+        update(10.0);
+        await check(false);
+      });
 
-      update(10.0);
-      await check(false);
-    });
+      testWidgets('state changes should be only on `pass` not on `equal or pass`', (tester) async {
+        await initialize(tester);
 
-    testWidgets('state changes should be only on `pass` not on `equal or pass`', (tester) async {
-      double _value = 10.0;
-      late StateSetter _updater;
+        // value = 10.0
+        await check(false);
 
-      Future<void> check(bool state) async {
-        await tester.pumpAndSettle();
-        expect(find.text('state: ${state ? 'on' : 'off'}'), findsOneWidget);
-      }
+        update(50.0);
+        await check(false);
 
-      void update(double newValue) {
-        _updater(() => _value = newValue);
-      }
+        // only on pass:
+        update(60.0);
+        await check(false);
 
-      await tester.pumpWidget(
-        MaterialApp(
-          title: 'Testing',
-          theme: ThemeData.light(),
-          home: Scaffold(
-            body: StatefulBuilder(
-              builder: (context, setState) {
-                _updater = setState;
+        update(70.0);
+        await check(true);
 
-                return OnOffControllerBuilder(
-                  initialState: false,
-                  value: _value,
-                  lowerLimit: 40.0,
-                  upperLimit: 60.0,
-                  builder: (context, state) {
-                    return Center(
-                      child: Text(
-                        'state: ${state ? 'on' : 'off'}',
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ),
-      );
+        update(50.0);
+        await check(true);
 
-      // value = 10.0
-      await check(false);
+        // only on pass:
+        update(40.0);
+        await check(true);
 
-      update(50.0);
-      await check(false);
-
-      // only on pass:
-      update(60.0);
-      await check(false);
-
-      update(70.0);
-      await check(true);
-
-      update(50.0);
-      await check(true);
-
-      // only on pass:
-      update(40.0);
-      await check(true);
-
-      update(30.0);
-      await check(false);
+        update(30.0);
+        await check(false);
+      });
     });
   });
 }
